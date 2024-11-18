152f8619e71ab73e068c548ae99a5f9a943eb28a
Kevyn Lima Freire <kevynlimafreire45@gmail.com>
2024-11-18 11:55:25 -0300

    main

--- perguntas.js
+++ perguntas.js
@@ -17,7 +17,37 @@ criaCartao(
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Língua Inglesa',
     'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Oi em inglês é Hi (RAI)'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'O Brasil foi descoberto por Pedro Álvares Cabral em 1500'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 5 + 5?',
+    '5 + 5 é igual a 10'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é fotossíntese?',
+    'Fotossíntese é o processo pelo qual as plantas produzem alimento usando luz solar'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual esporte é conhecido como o "esporte rei"?',
+    'O futebol é conhecido como o "esporte rei"'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci'
 )
