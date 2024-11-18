08e7431b7262dbe752155c5fc7ec0c76af0bf46a
Kevyn Lima Freire <kevynlimafreire45@gmail.com>
2024-11-18 12:16:45 -0300

    main

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,53 @@
 criaCartao(
-    'Tema CSS',
+    'programação',
     'O que é CSS',
     'É uma linguagem para estilizar páginas HTML'
 )
 
 criaCartao(
-    'Tema CSS',
+    'programação',
     'Para que serve o id o CSS',
     'Estilizar um elemento unico'
 )
 
 criaCartao(
-    'Tema CSS',
+    'programação',
     'Como mudar a cor de fundo com o CSS?',
-    'Use background-color '
+    'Use background - color '
 )
 
 criaCartao(
-    'tema HTML',
-    'Para que serve a tag <a>?',
+    'programação',
+    'Para que serve a tag < a >?',
     'Criar links'
 )
 
 criaCartao(
-    'tema HTML',
-    'O que é uma tag <div>?',
+    'programação',
+    'O que é uma tag < div >?',
     'Um contêiner de bloco para agrupar elementos'
 )
 
 criaCartao(
-    'tema HTML',
+    'programação',
     'Como criar um lista numerada?',
-    'Use a tag <ol> com <li>'
+    'Use a tag < ol > com < li >'
 )
 
 criaCartao(
-    'Tema JavaScript',
+    'programação',
     'O que é JavaScript?',
     'LInguagem para adicionar interatividade em páginas web'
 )
 
 criaCartao(
-    'Tema JavaScript',
+    'programação',
     'Como exibir uma mensagem de alerta?',
-    'Use alert()'
+    'Use alert( )'
 )
 
 criaCartao(
-    'Tema JavaScript',
+    'programação',
     'Para que serve o let no JavaScript?',
     'Declarar variaveis com escopo de bloco'
 )
