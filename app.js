83180d305167ef6dfcd3ebd8b5ac39192b7824e4
Kevyn Lima Freire <kevynlimafreire45@gmail.com>
2024-11-20 14:49:51 -0300

    main

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById("container");
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-    cartoes.forEach(cartao => {
-        const article = document.createElement("article");
-        article.classList.add("cartao");
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
 
-        article.innerHTML = `
-            <div class="cartao__conteudo">
-                <h3>${cartao.categoria}</h3>
-                <div class="cartao__conteudo__pergunta">
-                    <p>${cartao.pergunta}</p>
-                </div>
-                <div class="cartao__conteudo__resposta">
-                    <p>${cartao.resposta}</p>
-                </div>
-            </div>
-        `;
+    let respostaEstaVisivel = false
 
-        container.appendChild(article);
-    });
-});
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
+}
