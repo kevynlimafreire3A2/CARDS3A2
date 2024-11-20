bfd8764249a3d44d3418daed8e8c2e848a9a7ed6
Kevyn Lima Freire <kevynlimafreire45@gmail.com>
2024-11-20 14:49:29 -0300

    main

--- perguntas.js
+++ perguntas.js
@@ -1,11 +1,46 @@
-const cartoes = [
-    { categoria: 'Programação', pergunta: 'O que é CSS?', resposta: 'É uma linguagem para estilizar páginas HTML' },
-    { categoria: 'Programação', pergunta: 'Para que serve o id no CSS?', resposta: 'Estilizar um elemento único' },
-    { categoria: 'Programação', pergunta: 'Como mudar a cor de fundo com o CSS?', resposta: 'Use background-color' },
-    { categoria: 'Programação', pergunta: 'Para que serve a tag <a>?', resposta: 'Criar links' },
-    { categoria: 'Programação', pergunta: 'O que é uma tag <div>?', resposta: 'Um contêiner de bloco para agrupar elementos' },
-    { categoria: 'Programação', pergunta: 'Como criar uma lista numerada?', resposta: 'Use a tag <ol> com <li>' },
-    { categoria: 'Programação', pergunta: 'O que é JavaScript?', resposta: 'Linguagem para adicionar interatividade em páginas web' },
-    { categoria: 'Programação', pergunta: 'Como exibir uma mensagem de alerta?', resposta: 'Use alert()' },
-    { categoria: 'Programação', pergunta: 'Para que serve o let no JavaScript?', resposta: 'Declarar variáveis com escopo de bloco' }
-];
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
